6222cafb8687338be62f84315b4ff41edd07c5ce
unknown <thilllaimakesh98@gmail.com>
2021-04-01 12:48:56 +0530

    MDR AND SH COMPLETED

---
+++ public/SUBDIVISION/madurai.js
@@ -0,0 +1,19 @@
+var sub_div = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#7e7e7e",
+            "marker-size": "medium",
+            "marker-symbol": "circle-stroked",
+            "name": "Madurai"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                78.11957359313965,
+                9.919055804265168
+            ]
+        }
+    }]
+}
